fafb0fa881a29bd2386dc201ed97828f70bb173f
tahar <kaendesign@gmail.com>
2021-11-17 00:49:35 +0100

    mercredi17Nov2021

---
+++ 14-javascript/site-formulaire/assets/js/fichier.js
@@ -0,0 +1,6 @@
+
+function afficher(){
+    //var maVariable = document.getElementById("champ").value;
+    var nom = document.getElementById("exampleFormControlInput1").value;
+document.write(nom);
+}
